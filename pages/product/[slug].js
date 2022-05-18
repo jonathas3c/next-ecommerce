742924523b26f38c913e105470c2bb62f697f3f7
Jonathas Germano <jonathas.germano@hotmail.com>
2022-05-18 10:59:22 -0300

    feat: Add Buy Now feature, clean unused methods

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -15,7 +15,13 @@ import { client, urlFor } from '../../lib/client';
 const ProductDetails = ({ product, products }) => {
 	const { image, name, details, price } = product;
 	const [index, setIndex] = useState(0);
-	const { decQty, incQty, qty, onAdd } = useStateContext();
+	const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+
+	const handleBuyNow = () => {
+		onAdd(product, qty);
+
+		setShowCart(true);
+	};
 
 	return (
 		<div>
@@ -64,9 +70,7 @@ const ProductDetails = ({ product, products }) => {
 							<span className='minus' onClick={decQty}>
 								<AiOutlineMinus />
 							</span>
-							<span className='num' onClick=''>
-								{qty}
-							</span>
+							<span className='num'>{qty}</span>
 							<span className='plus' onClick={incQty}>
 								<AiOutlinePlus />
 							</span>
@@ -80,7 +84,7 @@ const ProductDetails = ({ product, products }) => {
 						>
 							Add to Cart
 						</button>
-						<button type='button' className='buy-now' onClick=''>
+						<button type='button' className='buy-now' onClick={handleBuyNow}>
 							Buy Now
 						</button>
 					</div>
