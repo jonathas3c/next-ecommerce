806e90398309912efc8d07fb93583b401f706cbf
Jonathas Germano <jonathas.germano@hotmail.com>
2024-04-16 21:59:09 -0300

    feat: set footer year info dinamically

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
 
 const Footer = () => {
+    const year = new Date().getFullYear()
     return (
         <div className="footer-container">
-            <p>2022 NXT Headphones All rights reserved</p>
+            <p>&copy; {year} NXT Headphones All rights reserved</p>
             <p className="icons">
                 <AiFillInstagram />
                 <AiOutlineTwitter />
